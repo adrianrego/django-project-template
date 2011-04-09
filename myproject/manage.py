599a47fde171fce3f6b61941ec660b6bc1be2233
Adrian Rego <adrian@foureyedmedia.com>
2011-04-08 20:57:01 -0400

    changes inspired by idangazit's epio_skel

--- myproject/manage.py
+++ myproject/manage.py
@@ -1,27 +1,8 @@
 #!/usr/bin/env python
 import os
-import sys
 from django import get_version
 from django.core.management import execute_from_command_line, LaxOptionParser
 from django.core.management.base import BaseCommand
-
-
-# Work out the project module name and root directory, assuming that this file
-# is located in the project source directory
-PROJECT_ROOT, PROJECT_MODULE_NAME = os.path.split(os.path.dirname(os.path.realpath(__file__)))
-
-# Check that the project module can be imported.
-try:
-    __import__(PROJECT_MODULE_NAME)
-except ImportError:
-    # Couldn't import the project, place it on the Python path and try again.
-    sys.path.append(PROJECT_ROOT)
-    try:
-        __import__(PROJECT_MODULE_NAME)
-    except ImportError:
-        sys.stderr.write("Error: Can't import the \"%s\" project module." %
-                         PROJECT_MODULE_NAME)
-        sys.exit(1)
 
 def has_settings_option():
     parser = LaxOptionParser(usage="%prog subcommand [options] [args]",
@@ -34,8 +15,7 @@ def has_settings_option():
     return bool(options.settings)
 
 if not has_settings_option() and not 'DJANGO_SETTINGS_MODULE' in os.environ:
-    settings_module = '%s.conf.local.settings' % PROJECT_MODULE_NAME
-    os.environ['DJANGO_SETTINGS_MODULE'] = settings_module
+    # Use local settings if other setting not passed in
+    os.environ['DJANGO_SETTINGS_MODULE'] = 'settings.local'
 
 execute_from_command_line()
-
--- myproject/settings/base.py
+++ myproject/settings/base.py
@@ -1,7 +1,6 @@
-# Django settings for myproject project.
-import os
+from unipath import FSPath as Path
 
-PROJECT_PATH = os.path.realpath(os.path.dirname(__file__))
+PROJECT_DIR = Path(__file__).absolute().ancestor(2) 
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -38,7 +37,7 @@ USE_L10N = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = os.path.join(PROJECT_PATH, '../public/uploads')
+MEDIA_ROOT = PROJECT_DIR.parent.child('public').child('uploads')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
@@ -49,7 +48,7 @@ MEDIA_URL = '/uploads/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = os.path.join(PROJECT_PATH, '../public/static')
+STATIC_ROOT = PROJECT_DIR.parent.child('public').child('static')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -58,14 +57,14 @@ STATIC_URL = '/static/'
 # URL prefix for admin static files -- CSS, JavaScript and images.
 # Make sure to use a trailing slash.
 # Examples: "http://foo.com/static/admin/", "/static/admin/".
-ADMIN_MEDIA_PREFIX = '/static/admin/'
+ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    os.path.join(PROJECT_PATH, 'static'),
+    PROJECT_DIR.child('static')
 )
 
 # List of finder classes that know how to find static files in
@@ -94,13 +93,13 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
 )
 
-ROOT_URLCONF = 'myproject.urls'
+ROOT_URLCONF = 'urls'
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    os.path.join(PROJECT_PATH, 'templates'),
+    PROJECT_DIR.child('templates')
 )
 
 INSTALLED_APPS = (
@@ -113,7 +112,6 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     'django.contrib.admindocs',
     'south',
-
 )
 
 # A sample logging configuration. The only tangible logging

10f345d95cdd6bf8b32a60b977a6a73159c7860a
Adrian Rego <arego320@gmail.com>
2011-09-07 22:07:50 -0400

    pylint and removing references to other project

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 from setuptools import setup, find_packages
 
-setup(name='presence',
+setup(name='myproject',
       version='0.1',
       packages=find_packages(),
-      package_data={'presence': ['bin/*.*',]},
-      exclude_package_data={'presence': ['bin/*.pyc']},
-      scripts=['presence/bin/manage.py'])
+      package_data={'myproject': ['bin/*.*', 'static/*.*', 'templates/*.*']},
+      exclude_package_data={'myproject': ['bin/*.pyc']},
+      scripts=['myproject/bin/manage.py'])
